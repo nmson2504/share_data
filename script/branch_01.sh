a5217ba2664fdffa43713c821b569618af069a1e
THINKPAD-T480S\Thinkpad T480s <you@example.com>
2024-05-03 11:03:16 +0700

    first commit

---
+++ script/branch_01.sh
@@ -0,0 +1,30 @@
+# delete all(include .git)
+rm -rf -- ..?* .[!.]* * 
+alias g_a='git log --all --decorate --oneline  --graph'
+alias g='git log --decorate --oneline  --graph' 
+
+git init
+
+touch m1.txt
+git add .
+git commit -m'C1 - master'
+sleep 1
+touch m2.txt
+git add .
+git commit -m'C2 - master'
+sleep 1
+
+git branch hotfix
+git checkout hotfix
+
+touch h1.txt
+git add .
+git commit -m'C3 - hotfix'
+sleep 1
+touch h2.txt
+git add .
+git commit -m'C4 - hotfix'
+sleep 1
+touch h3.txt
+git add .
+git commit -m'C5 - hotfix'
